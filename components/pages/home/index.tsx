3875bf1cd68e6195f8aa8c64d2dd910e965c4b07
Roe Greene <roethedev@gmail.com>
2023-10-26 23:29:40 +0000

    fix bug with formatter

--- components/pages/home/index.tsx
+++ components/pages/home/index.tsx
@@ -2,7 +2,8 @@ const HomePage = () => {
     return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
     Hello Roe
-  </main>)
+  </main>
+  )
 }
 
 export default HomePage
