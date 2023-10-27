5a0bf9f7c29c03f1ee42cb81f0f9900c6264f698
Roe Greene <roethedev@gmail.com>
2023-10-27 02:03:43 +0000

    test

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,8 +1,8 @@
 import Layout from './_components/layout'
 export default function Home() {
   return (
-      <Layout>
-        <h1>Hello y&apos;all</h1>
-      </Layout>
+    <Layout>
+      <h1>Hello y&apos;all</h1>
+    </Layout>
   )
 }
