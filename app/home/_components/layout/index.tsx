e199a5b9c26244b1798269648ca13ba6ab8a65cf
Roe Greene <roethedev@gmail.com>
2023-10-27 01:47:16 +0000

    tweak linter

--- app/home/_components/layout/index.tsx
+++ app/home/_components/layout/index.tsx
@@ -1,10 +1,11 @@
 import { PropsWithChildren } from "react"
 
 const Layout:React.FC<PropsWithChildren> = ({children}) => {
-    return <main className="flex min-h-screen flex-col items-center justify-between p-24">
-   
+    return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
         {children}
     </main>
+)
 }
 
 export default Layout
