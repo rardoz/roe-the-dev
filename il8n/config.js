0dbffe3a8ff677de7dcf787617fe71bc09f2f3e6
Roe Greene <roethedev@gmail.com>
2023-11-10 03:52:34 +0000

    step 1: setup intl

---
+++ il8n/config.js
@@ -0,0 +1,6 @@
+// Read: https://nextjs.org/docs/pages/building-your-application/routing/internationalization
+
+module.exports = {
+  locales: ['en-US', 'en-ES', 'en-CA', 'en-GB'],
+  defaultLocale: 'en-US',
+}
--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
+
+const il8n = require('./il8n/config.js')
+
 const nextConfig = {
+  il8n,
   async rewrites() {
     return [
       {
