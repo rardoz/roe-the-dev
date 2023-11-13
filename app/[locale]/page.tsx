10b768aad494c2edfc1c8fd27db48abbef42843c
Roe Greene <roethedev@gmail.com>
2023-11-13 03:24:53 +0000

    cleanup nav

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -2,10 +2,31 @@ import { useTranslations } from 'next-intl'
 import Nav from '../_components/nav'
 import NavLinks from '../_components/nav/nav-links'
 import H1 from '../_components/h1'
+import { PiTelevisionBold } from 'react-icons/pi'
+import { GiEvilBook } from 'react-icons/gi'
+import { SiTestinglibrary } from 'react-icons/si'
+import Link from 'next/link'
 
-export default function Home() {
+export default function Home(props: { params?: { locale: string } }) {
   const homeTranslations = useTranslations('Home')
   const navTranslations = useTranslations('Nav')
+  const links = [
+    {
+      label: navTranslations('portfolio'),
+      icon: <PiTelevisionBold className="mr-1.5" />,
+      url: `/${props.params?.locale || ''}/portfolio`,
+    },
+    {
+      label: navTranslations('blog'),
+      icon: <GiEvilBook className="mr-1.5" />,
+      url: `/${props.params?.locale || ''}/blog`,
+    },
+    {
+      label: navTranslations('experiments'),
+      icon: <SiTestinglibrary className="mr-1.5" />,
+      url: `/${props.params?.locale || ''}/experiments`,
+    },
+  ]
   return (
     <>
       <Nav
@@ -15,25 +36,18 @@ export default function Home() {
         }}
       >
         <NavLinks>
-          <a
-            href="#"
-            className="block py-2 px-3 text-white bg-blue-700 rounded dark:bg-blue-600"
-            aria-current="page"
-          >
-            {navTranslations('portfolio')}
-          </a>
-          <a
-            href="#"
-            className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-          >
-            {navTranslations('blog')}
-          </a>
-          <a
-            href="#"
-            className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white"
-          >
-            {navTranslations('experiments')}
-          </a>
+          {links.map(({ url, label, icon }, index) => {
+            return (
+              <Link
+                key={index}
+                href={url}
+                className="mx-4 mb-4 py-2 px-3 flex items-center transition-colors duration-500 hover:bg-pink-600 text-white bg-purple-700 rounded-full dark:bg-blue-600"
+              >
+                {icon}
+                {label}
+              </Link>
+            )
+          })}
         </NavLinks>
       </Nav>
       <main className="h-100 flex flex-col items-center justify-between p-4">
