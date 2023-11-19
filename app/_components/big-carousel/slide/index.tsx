9d99d8c05c6d5eb42c485402b34143c4eb3731c5
Roe Greene <roethedev@gmail.com>
2023-11-19 06:44:51 +0000

    style carousel

--- app/_components/big-carousel/slide/index.tsx
+++ app/_components/big-carousel/slide/index.tsx
@@ -12,7 +12,7 @@ const CarouselSlide: React.FC<NormalizedBlogData> = ({
 }) => {
   return (
     <div className="h-full relative flex justify-center items-center">
-      <div className="h-full w-full absolute -z-10">
+      <div className="h-full w-full absolute -z-10 top-0 left-0">
         <Image
           src={featuredImage?.url || ''}
           alt={featuredImage?.description || featuredImage?.title || ''}
@@ -21,11 +21,16 @@ const CarouselSlide: React.FC<NormalizedBlogData> = ({
           className="object-cover object-center"
         />
       </div>
-      <div>
-        <h2>{title}</h2>
-        <p>{description}</p>
-        <div>
-          <Link target={hardRoute ? '_blank' : undefined} href={slug || ''}>
+      <div className="h-full w-full absolute top-0 left-0  bg-gradient-to-t from-slate-900 bg-opacity-20 z-0"></div>
+      <div className="z-10">
+        <h2 className="text-5xl font-extrabold text-pink-500">{title}</h2>
+        <p className="text-4xl mt-2 text-slate-50">{description}</p>
+        <div className="mt-4 text-right underline underline-offset-4 font-bold ">
+          <Link
+            className="text-xl text-pink-500 hover:bg-pink-500 duration-500 transition-colors p-2 hover:text-slate-50"
+            target={hardRoute ? '_blank' : undefined}
+            href={slug || ''}
+          >
             See&nbsp;
             {title}
           </Link>
