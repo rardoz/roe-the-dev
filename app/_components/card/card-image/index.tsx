80ded8f963818a7e2f83e9de44da9c33f07fa1d6
Roe Greene <Roe@AmericatomModel.localdomain>
2023-11-20 01:42:04 -0800

    fix object-fit in card

--- app/_components/card/card-image/index.tsx
+++ app/_components/card/card-image/index.tsx
@@ -4,7 +4,7 @@ import { FC } from 'react'
 const CardImage: FC<{ src: string; alt: string }> = ({ src, alt }) => {
   return (
     <div className="relative min-h-96 min-h-[24rem]">
-      <Image fill objectFit="cover" src={src} alt={alt} />
+      <Image fill className="object-cover" src={src} alt={alt} />
     </div>
   )
 }
