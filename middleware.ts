fcae0b97757e859744e09051db59afc6b000b9bb
Roe Greene <Roe@AmericatomModel.localdomain>
2023-11-25 22:01:12 -0800

    fix intl routing

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,16 @@
 import createMiddleware from 'next-intl/middleware'
 import messagesConfig from './messages/config'
 
-export default createMiddleware(messagesConfig)
+export default createMiddleware({
+  // A list of all locales that are supported
+  locales: messagesConfig.locales,
+  // Used when no locale matches
+  defaultLocale: messagesConfig.defaultLocale,
+  localePrefix: messagesConfig.localePrefix,
+  pathnames: messagesConfig.pathnames,
+})
 
 export const config = {
-  // Skip all paths that should not be internationalized. This example skips
-  // certain folders and all pathnames with a dot (e.g. favicon.ico)
+  // Match only internationalized pathnames
   matcher: ['/((?!api|_next|_vercel|.*\\..*).*)'],
 }
--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
 /** @type {import('next').NextConfig} */
-
 const withNextIntl = require('next-intl/plugin')(
   // This is the default (also the `src` folder is supported out of the box)
   './messages/i18n.ts',
