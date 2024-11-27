3a59a37e2b2c696b3889db359ff3b7aa3a814366
Roe Greene <Roe@AmericatomModel.localdomain>
2024-11-26 22:59:42 -0700

    set request locale

--- app/[locale]/lottie-winters-kinky-salad-model-game/page.tsx
+++ app/[locale]/lottie-winters-kinky-salad-model-game/page.tsx
@@ -1,8 +1,10 @@
 'use client'
+import { unstable_setRequestLocale } from 'next-intl/server'
 import LottieGame from '../../_components/lottie-game'
 import './styles.css'
 
 export default async function LWKSMG() {
+  unstable_setRequestLocale('en-US')
   return (
     <div className="lottie-layout">
       <LottieGame />
